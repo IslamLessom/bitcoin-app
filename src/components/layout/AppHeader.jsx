70214af965ee57a7536635e692380962817f3838
Islam <mem.m.1979@mail.ru>
2024-05-06 13:24:48 +0300

    header select and button

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,16 +1,67 @@
-import { Layout } from "antd";
+import { Button, Layout, Select, Space } from 'antd';
+import { useCrypto } from '../../context/crypto-context';
 
 const headerStyle = {
+    width: '100%',
     textAlign: 'center',
-    color: '#fff',
     height: 60,
-    paddingInline: 48,
-    lineHeight: '64px',
-    backgroundColor: '#4096ff',
-};
+    padding: '1rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+}
+
+const handleChange = (value) => {
+    console.log(`selected ${value}`);
+}
+const options = [
+    {
+        label: 'China',
+        value: 'china',
+        emoji: '🇨🇳',
+        desc: 'China (中国)',
+    },
+    {
+        label: 'USA',
+        value: 'usa',
+        emoji: '🇺🇸',
+        desc: 'USA (美国)',
+    },
+    {
+        label: 'Japan',
+        value: 'japan',
+        emoji: '🇯🇵',
+        desc: 'Japan (日本)',
+    },
+    {
+        label: 'Korea',
+        value: 'korea',
+        emoji: '🇰🇷',
+        desc: 'Korea (韩国)',
+    },
+];
 
 export const AppHeader = () => {
+    const { crypto } = useCrypto()
     return (
-        <Layout.Header style={headerStyle}>Header</Layout.Header>
+        <Layout.Header style={headerStyle}>
+            <Select
+                style={{
+                    width: 250
+                }}
+                value='press / to open'
+                options={crypto.map((coin) => ({
+                    label: coin.name,
+                    value: coin.id,
+                    icon: coin.icon
+                }))}
+                optionRender={(option) => (
+                    <Space>
+                        <img style={{width: 20}} src={option.data.icon} alt={option.data.label} /> {option.data.label}
+                    </Space>
+                )}
+            />
+            <Button type='primary'>Add Assets</Button>
+        </Layout.Header>
     )
 }
